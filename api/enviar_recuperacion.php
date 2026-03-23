09c3ed75eb564a6026600a6dc7e62f5c42fca0ac
Luis Lobos G <llobos@gltcomex.com>
2026-03-23 09:42:13 -0300

    feat:+ api/enviar_recuperacion.php-5

--- api/enviar_recuperacion.php
+++ api/enviar_recuperacion.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once __DIR__ . '../includes/config.php';
+require_once __DIR__ . '/../includes/config.php';
 
 try {
     $nombre = trim($_POST['nombre'] ?? '');
